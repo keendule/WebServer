c4cf48927f0df7d9f2ace558d061beac86b8e576
keendule <keendule@gmail.com>
2018-06-27 06:55:24 +0700

    add jdbcconnectionPool

--- src/dbService/dao/UserDaoJDBCImpl.java
+++ src/dbService/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,8 @@
 package dbService.dao;
 
+import dbService.DBConnection.JDBCConnectionPool;
 import dbService.Executors.Executor;
-import dbService.services.DBHelper;
+import dbService.DBConnection.DBHelper;
 import entity.UserEntity;
 
 import java.sql.Connection;
@@ -11,10 +12,11 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDAO {
 
-    Connection connection = DBHelper.getConnection();
+    private JDBCConnectionPool jdbcConnectionPool = DBHelper.getConnectionPoll();
 
     @Override
     public List<UserEntity> getAllUsers() {
+        Connection connection = jdbcConnectionPool.takeOut();
         Executor executor = new Executor(connection);
         List<UserEntity> users;
         try {
@@ -40,12 +42,15 @@ public class UserDaoJDBCImpl implements UserDAO {
         return users;
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            jdbcConnectionPool.takeIn(connection);
         }
         return null;
     }
 
     @Override
     public void updateUser(UserEntity user) {
+        Connection connection = jdbcConnectionPool.takeOut();
         Executor executor = new Executor(connection);
         try {
             executor.execUpdatePreparedStatementQuery("UPDATE users SET " +
@@ -53,21 +58,27 @@ public class UserDaoJDBCImpl implements UserDAO {
                     user.getLogin(),user.getPassword(),user.getName(),user.getId());
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            jdbcConnectionPool.takeIn(connection);
         }
     }
 
     @Override
     public void deleteUser(int id) {
+        Connection connection = jdbcConnectionPool.takeOut();
         Executor executor = new Executor(connection);
         try {
             executor.execUpdatePreparedStatementQuery("DELETE FROM users WHERE id = ?",id);
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            jdbcConnectionPool.takeIn(connection);
         }
     }
 
     @Override
     public void createUser(UserEntity user) {
+        Connection connection = jdbcConnectionPool.takeOut();
         Executor executor = new Executor(connection);
         try {
             executor.execUpdatePreparedStatementQuery("INSERT INTO users " +
@@ -75,11 +86,14 @@ public class UserDaoJDBCImpl implements UserDAO {
                     user.getLogin(),user.getPassword(),user.getName());
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            jdbcConnectionPool.takeIn(connection);
         }
     }
 
     @Override
     public UserEntity getUser(int id) {
+        Connection connection = jdbcConnectionPool.takeOut();
         Executor executor = new Executor(connection);
         try {
            return executor.execSelectPreparedStatementQuery("Select * FROM users" +
@@ -104,6 +118,8 @@ public class UserDaoJDBCImpl implements UserDAO {
             },id);
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            jdbcConnectionPool.takeIn(connection);
         }
 
         return null;
